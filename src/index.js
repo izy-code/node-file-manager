9b127c08d4cceee483805ee723770e44f466c4cd
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-13 23:35:04 +0300

    feat: add readline prompt and new line handler

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import { createInterface } from 'readline/promises';
 import { printWelcomeMessage, printFarewellMessage } from './utils/user.js';
+import { handleInput } from './commands/input-handler.js';
+import { printCurrentDirectory, setStartingDir } from './utils/directory-path.js';
+import { getColoredText } from './utils/color-output.js';
 
 const init = () => {
-    printWelcomeMessage();
-
     const readline = createInterface({
         input: process.stdin,
         output: process.stdout,
+        prompt: getColoredText('BLUE', '> '),
     });
 
+    printWelcomeMessage();
+    setStartingDir();
+    printCurrentDirectory();
+    readline.prompt();
+
     readline.on('close', printFarewellMessage);
-    // readline.on('line', async (input) => await handleInput(input));
+    readline.on('line', async (input) => {
+        await handleInput(input);
+        readline.prompt();
+    });
 };
 
 init();
