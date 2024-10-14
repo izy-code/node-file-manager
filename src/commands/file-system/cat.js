e62d894e874ab5a896ee59ca6f33fb97446f962c
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-14 14:06:27 +0300

    fix: use events instead of pipeline in cat

--- src/commands/file-system/cat.js
+++ src/commands/file-system/cat.js
@@ -1,13 +1,18 @@
 import { resolve } from 'path';
 import { createReadStream } from 'fs';
-import { pipeline } from 'stream/promises';
+import { EOL } from 'os';
 import { getCurrentDir } from '../../utils/directory-path.js';
 import { getArgByNumber } from "../../utils/command-input.js";
 
 export const cat = async (args) => {
     const argPath = getArgByNumber(args, 0);
     const resolvedPath = resolve(getCurrentDir(), argPath);
-    const sourceStream = createReadStream(resolvedPath);
 
-    await pipeline(sourceStream, process.stdout, { end: false });
+    return new Promise((resolve, reject) => {
+        const readStream = createReadStream(resolvedPath);
+
+        readStream.on('data', (chunk) => process.stdout.write(`${EOL}${chunk.toString()}`));
+        readStream.on('end', resolve);
+        readStream.on('error', (error) => reject(error));
+    });
 };
